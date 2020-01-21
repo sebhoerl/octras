a629ef98d70b466ad9b058dfddce120f4b43a70d
anmakaro <anmakaro@inf.ethz.ch>
2020-01-21 13:41:06 +0100

    updated setup for custom emukit

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ setup(
     packages = find_packages(where = "src"),
     install_requires = [
         "GPy>=1.9.9",
-        "emukit>=0.4.7",
+        "emukit==1.4.1", # github version with the link in dependency_links
         "scipy==1.1.0", # Necessary for emukit
         "pandas>=0.25.3",
         "matplotlib>=3.1.2",
         "pyDOE>=0.3.8",
         "PyYAML>=5.1.2",
     ],
+    dependency_links=['https://github.com/Avidereta/emukit/tarball/my_contributions#egg=emukit-1.4.1'],
     extras_require = {
         "test": ["pytest>=5.3.1"], "example": ["pandas>=0.25.3"]
     },
